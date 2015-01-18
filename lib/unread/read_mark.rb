642e74a97f18c3c318c007fad61a52ea5b7b0e73
mihailpaleologu <mihailpaleologu@yahoo.com>
2015-01-18 22:25:43 +0200

    Update read_mark.rb

--- lib/unread/read_mark.rb
+++ lib/unread/read_mark.rb
@@ -3,8 +3,7 @@ class ReadMark < ActiveRecord::Base
   attr_accessible :readable_id, :user_id, :readable_type, :timestamp
 
   validates_presence_of :user_id, :readable_type
-  sync :all
-  sync_scope :by_user, ->(user) { where(user_id: user.id)}
+  
   
   scope :global, lambda { where(:readable_id => nil) }
   scope :single, lambda { where('readable_id IS NOT NULL') }
